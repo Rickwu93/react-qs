79899346593710ea0ac902b8728e4262971e20ed
Ricky Wu <rickywu209@gmail.com>
2023-03-28 00:40:37 -0600

    simple useState - + counter

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
+import { useState } from 'react';
 import './App.css';
 
-const Person = (props) => {
-  return (
-    <>
-    <h1>Name: {props.name}</h1>
-    <h2>Last Name: {props.lastName}</h2>
-    <h2>Age: {props.age}</h2>
-    </>
-  )
-}
+
 
 const App = () => {
-
+  const [counter, setCounter] = useState(0);
 
   return (
     <div className="App">
-      <Person name={'Jane'} lastName={'Doe'} age={25}/>
-      <Person />
-      <Person />
-      <Person />
-      <Person />
+      <button onClick={() => setCounter((prevCount) => prevCount - 1)}>-</button>
+      <h1>{counter}</h1>
+      <button onClick={() => setCounter((prevCount) => prevCount + 1)}>+</button>
     </div>
   );
 }
