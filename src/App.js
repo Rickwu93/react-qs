c9c1db1a66f9b1b8c48cc124b6914d8a7188050b
Ricky Wu <rickywu209@gmail.com>
2023-03-27 18:51:05 -0600

    conditional rendering

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import './App.css';
 
 const App = () => {
-  const name = 'John';
-  const isNameShowing = true;
+  const name = null;
+  
 
   return (
     <div className="App">
-      <h1>hello, {isNameShowing ? name : 'someone'}! </h1>
+      <h1>hello, {2 + 2}! </h1>
+      {name ? (
+        <>
+          test
+        </>
+      ) : (
+        <h1>test2</h1>
+      )}
     </div>
   );
 }
