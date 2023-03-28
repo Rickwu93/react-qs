672144652127cbf84b822be66296e34fc74754bc
Ricky Wu <rickywu209@gmail.com>
2023-03-27 21:53:20 -0600

    simple person component

--- src/App.js
+++ src/App.js
@@ -1,22 +1,22 @@
 import './App.css';
 
+const Person = () => {
+  return (
+    <>
+    <h1>Name: John</h1>
+    <h2>Last Name: Doe</h2>
+    <h2>Age: 30</h2>
+    </>
+  )
+}
+
 const App = () => {
-  const name = null;
+  const name = 'Ricky';
   
 
   return (
     <div className="App">
       <h1>hello, {2 + 2}! </h1>
-      {name ? (
-        <>
-          test
-        </>
-      ) : (
-        <>
-        <h1>test2</h1>
-        <h2>There is no name</h2>
-        </>
-      )}
     </div>
   );
 }
