82a4f8098fc605e6425f660ee0a6813757fde6e8
Ricky Wu <rickywu209@gmail.com>
2023-03-29 03:03:00 -0600

    adding in movie poster and movie information

--- src/App.js
+++ src/App.js
@@ -51,7 +51,12 @@ const App = () => {
                     </div>
 
                     <div>
-                        <img src={movie1.Poster} alt={movie1.Title}/>
+                        <img src={movie1.Poster !== 'N/A' ? movie1.Poster : 'https//via.placeholder.com/400'} alt={movie1.Title}/>
+                    </div>
+
+                    <div>
+                        <span>{movie1.Type}</span>
+                        <h3>{movie1.Title}</h3>
                     </div>
                 </div>
             </div>
