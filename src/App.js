ad9e03b1f7d351612c06601a331a84fbac31e223
Ricky Wu <rickywu209@gmail.com>
2023-03-29 02:55:38 -0600

    adding fake object data

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
 import { useEffect } from "react";
 
+import SearchIcon from "./search.svg";
+import "./App.css";
 
 const API_URL = 'http://www.omdbapi.com?apikey=8733c30d';
+
+const movie1 = {
+        "Title": "Superman, Spiderman or Batman",
+        "Year": "2011",
+        "imdbID": "tt2084949",
+        "Type": "movie",
+        "Poster": "https://m.media-amazon.com/images/M/MV5BMjQ4MzcxNDU3N15BMl5BanBnXkFtZTgwOTE1MzMxNzE@._V1_SX300.jpg"
+}
 
 const App = () => {
 
@@ -18,7 +28,34 @@ const App = () => {
     }, []);
 
     return (
-        <h1>App</h1>
+        <div className="app">
+            <h1>Movies Hub</h1>
+
+            <div className="search">
+                <input
+                    placeholder="Search for movies"
+                    value="superman"
+                    onChange={() => {}}
+                />
+                <img
+                src={SearchIcon}
+                alt="search"
+                onClick={() => {}}
+                />
+            </div>
+
+            <div className="container">
+                <div className="movie">
+                    <div>
+                        <p>{movie1.Year}</p>
+                    </div>
+
+                    <div>
+                        <img src={movie1.Poster} alt={movie1.Title}/>
+                    </div>
+                </div>
+            </div>
+        </div>
     )
 }
 
